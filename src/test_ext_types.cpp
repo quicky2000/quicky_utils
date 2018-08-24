e5c3069712220ef2c9b5197b1e1832a3054d76f2
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2018-08-24 09:08:55 +0200

    Add intensive tests of ext_int relational operators

--- src/test_ext_types.cpp
+++ src/test_ext_types.cpp
@@ -284,6 +284,78 @@ namespace quicky_utils
             l_ok &= quicky_test::check_ostream_operator(l_iter.second, l_reference, quicky_test::auto_message(__FILE__, __LINE__));
         }
 
+        // Check == operator
+        for(auto l_iter1: l_test_values)
+        {
+            for(auto l_iter2: l_test_values)
+            {
+                l_ok &= quicky_test::check_expected(l_iter1.second == l_iter2.second,
+                                                    l_iter1.first == l_iter2.first,
+                                                    (std::string)l_iter1.second + " == " + (std::string)l_iter2.second
+                                                   );
+            }
+        }
+
+        // Check != operator
+        for(auto l_iter1: l_test_values)
+        {
+            for(auto l_iter2: l_test_values)
+            {
+                l_ok &= quicky_test::check_expected(l_iter1.second != l_iter2.second,
+                                                    l_iter1.first != l_iter2.first,
+                                                    (std::string)l_iter1.second + " != " + (std::string)l_iter2.second
+                                                   );
+            }
+        }
+
+        // Check < operator
+        for(auto l_iter1: l_test_values)
+        {
+            for(auto l_iter2: l_test_values)
+            {
+                l_ok &= quicky_test::check_expected(l_iter1.second < l_iter2.second,
+                                                    l_iter1.first < l_iter2.first,
+                                                    (std::string)l_iter1.second + " < " + (std::string)l_iter2.second
+                                                   );
+            }
+        }
+
+        // Check <= operator
+        for(auto l_iter1: l_test_values)
+        {
+            for(auto l_iter2: l_test_values)
+            {
+                l_ok &= quicky_test::check_expected(l_iter1.second <= l_iter2.second,
+                                                    l_iter1.first <= l_iter2.first,
+                                                    (std::string)l_iter1.second + " <= " + (std::string)l_iter2.second
+                                                   );
+            }
+        }
+
+        // Check > operator
+        for(auto l_iter1: l_test_values)
+        {
+            for(auto l_iter2: l_test_values)
+            {
+                l_ok &= quicky_test::check_expected(l_iter1.second > l_iter2.second,
+                                                    l_iter1.first > l_iter2.first,
+                                                    (std::string)l_iter1.second + " > " + (std::string)l_iter2.second
+                                                   );
+            }
+        }
+
+        // Check >= operator
+        for(auto l_iter1: l_test_values)
+        {
+            for(auto l_iter2: l_test_values)
+            {
+                l_ok &= quicky_test::check_expected(l_iter1.second >= l_iter2.second,
+                                                    l_iter1.first >= l_iter2.first,
+                                                    (std::string)l_iter1.second + " >= " + (std::string)l_iter2.second
+                                                   );
+            }
+        }
+
         // Experiments around int type
         int16_t l_int = -1;
         for(unsigned int l_index = 0; l_index < 16; ++ l_index)
